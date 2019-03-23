5dea85b2fab4e2e0123747c862dc6514e2000f5c
Glen Mould <mouldlgen@gmail.com>
2019-03-23 14:03:39 +0000

    V1.0
    V1.0

--- venvRSTL/rstl/rstl/settings.py
+++ venvRSTL/rstl/rstl/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'frontend', 
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIR = [os.path.join(BASE_DIR, "frontend/teemplate/templates/static"),]
